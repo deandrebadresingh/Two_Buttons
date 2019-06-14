8761f85fef02e79542207d62602adea5110b3b4a
DeAndre <de.andre.badresingh@brooklynsteamcenter.org>
2019-06-14 13:44:19 -0400

    TwoButtons

--- Two_Buttons/ViewController.swift
+++ Two_Buttons/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var change: UILabel!
+    @IBOutlet var screen: UIView!
+    @IBAction func button2(_ sender: Any) {
+    screen.backgroundColor = UIColor.green
+    change.text = "HELLOWORLD"
+    }
+    @IBAction func button1(_ sender: Any) {
+        screen.backgroundColor =  UIColor.orange
+        change.text = "WORLDHELLO"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
