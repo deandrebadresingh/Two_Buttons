b7546b5fa2cd56518c978244ab369a26168d3069
DeAndre <de.andre.badresingh@brooklynsteamcenter.org>
2019-06-14 13:24:08 -0400

    Initial Commit

---
+++ Two_Buttons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two_Buttons
+//
+//  Created by Genesis on 6/14/19.
+//  Copyright © 2019 DeAndre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
